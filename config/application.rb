7ebd32028eefa232bb10b5548430927e036d381f
Mottttton <mottttton.webdev@gmail.com>
2024-03-12 12:35:20 +0900

    add feature: Active Job

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module RailsFunctionsTaskApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
+    config.active_job.queue_adapter = :sidekiq
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
